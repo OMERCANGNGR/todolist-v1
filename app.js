d2346c33303497085ec92dd7b1ebba4bfccbc2b2
ÖMER <omercangungor45@gmail.com>
2023-06-27 11:57:14 +0300

    index.html file changed to index.ejs to apply ejs edits.

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 let ejs = require('ejs');
-
+var name=  2+5;
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.render('index', {name: name});
 });
 
 app.listen(3000,function(){
