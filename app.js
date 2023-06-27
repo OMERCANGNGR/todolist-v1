3943f53a4f05e0d20a6d096059ef27bd3b202990
ÖMER <omercangungor45@gmail.com>
2023-06-27 13:36:47 +0300

    new date variable and switch statement to insert the day

--- app.js
+++ app.js
@@ -3,10 +3,42 @@ const bodyParser = require('body-parser');
 
 const app = express();
 let ejs = require('ejs');
-var name=  2+5;
+
 app.set('view engine', 'ejs');
+
+var date = new Date();
+var day= date.getDay();
+var dayName="";
+switch (day) {
+    case 0:
+        dayName="Sunday";
+        break;
+    case 1:
+        dayName="Monday";
+        break;
+    case 2:
+        dayName="Tuesday";
+        break;
+    case 3:
+        dayName="Wednesday";
+        break;
+    case 4:
+        dayName="Thursday";
+        break;
+    case 5:
+        dayName="Friday";
+        break;
+    case 6:
+        dayName="Saturday";
+        break;
+
+    default:
+         console.log("Something went wrong");
+        break;
+}
+
 app.get('/', (req, res) => {
-    res.render('index', {name: name});
+    res.render('index', {kindOfDay: dayName});
 });
 
 app.listen(3000,function(){
